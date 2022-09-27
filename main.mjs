b0bf2fa84104f06e4e03e2e924dfbb179031aa70
Luis Davi Vieira SIlva <85641308+LuisDaviOFC@users.noreply.github.com>
2022-09-27 16:45:21 -0300

    finalizado

---
+++ main.mjs
@@ -0,0 +1,8 @@
+import { mostraCidade, mostraIdade, mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraIdade("Luis Davi", 22));
+alert(mostraIdade("Luis Davi", 22));
+console.log(mostraCidade("Luis Davi", "Itapuranga(GO)"));
+alert(mostraCidade("Luis Davi", "Itapuranga(GO)"));
+console.log(mostraHobby("Luis Davi", "Jogos Eletrônicos"));
+alert(mostraHobby("Luis Davi", "Jogos Eletrônicos"));
